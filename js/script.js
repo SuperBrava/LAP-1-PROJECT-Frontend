8550bffead6e00b2f00238bf0dae136b8147552e
Aleksandra Opacic <aleksandraopacic@hotmail.co.uk>
2022-06-08 10:26:57 +0100

    still working on the gif stuff

--- js/script.js
+++ js/script.js
@@ -14,7 +14,29 @@ function init() {
         ev.preventDefault();
         let url= `https://api.giphy.com/v1/gifs/trending?api_key=${apikey}&limit=5`;
         let str= document.getElementById("search").value.trim();
-    })
+        url = url.concat(str);
+        console.log(url);
+        fetch(url)
+        .then(response => response.json())
+        .then(content => {
+            console.log(content.data);
+            console.log("META", content.meta);
+            let fig = document.createElement('figure');
+            let img = document.createElement('img');
+            let fc = document.createElement('figcaption');
+            img.src = content.data[0].images.downsized.url;
+            img.alt = content.data[0].title;
+            fc.textContent = content.data[0].title;
+            fig.appendChild(img);
+            fig.appendChild(fc);
+            let out = document.querySelector('.out');
+            out.insertAdjacentElement('afterbegin', fig);
+
+        })
+            .catch(err => {
+                console.error(err);
+     })                               
+    });
 }
 
 //just working on the gify api 
