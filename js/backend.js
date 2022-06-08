91a909cb81fd80676dd32ad3249c64e0186f2702
Stu <stuart.judd.bsc@gmail.com>
2022-06-08 10:11:42 +0100

    FE/BE Connection

---
+++ js/backend.js
@@ -0,0 +1,8 @@
+
+
+//Resolving Server Connection Issue-->
+  fetch('https://api.allorigins.win/raw?url=https://portfolio-project-1-backend.herokuapp.com/get/readPost')
+    .then(r => r.json())
+    .then(r => console.log(r))
+    .catch(console.warn);
+
