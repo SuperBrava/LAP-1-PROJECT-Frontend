9b02d1faf07629f0e379b5606274f1fe4bda2479
Aleksandra Opacic <aleksandraopacic@hotmail.co.uk>
2022-06-09 10:20:56 +0100

    commit

--- frontend.spec.js
+++ frontend.spec.js
@@ -1,17 +1,4 @@
 
-const jsdom = require("jsdom");
-const dom = new JSDOM(`<!DOCTYPE html><body><p id ="main" My First JSDOM!</p></body>`);
-console.log(dom.window.document.getElementById("main").textContent);
-
-
-
-
-
-/*const init = require('./js/script')
-
-test('properly calls the giphy api', () => {
-    expect(init(url)).toBe(`https://api.giphy.com/v1/gifs/search?api_key=${APIKEY}&limit=1&q=`)
-})
 
 const postInstance = require('./js/script')
 
@@ -24,5 +11,3 @@ test('post instance to create upVote', () => {
 test('properly creates a post instance', () => {
 
 })
-
-*/
